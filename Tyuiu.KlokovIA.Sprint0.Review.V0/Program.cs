cb6fcde82c3e13ac16414abf44e0d22c215f7e48
ivan <ivan.proekt00@mail.ru>
2023-10-19 02:37:15 +0500

    Add project files.

---
+++ Tyuiu.KlokovIA.Sprint0.Review.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KlokovIA.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.KlokovIA.Sprint0.Review.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            Console.WriteLine("X = " + x);
+            int y = 4;
+            Console.WriteLine("V = " + y);
+            int z = 6;
+            Console.WriteLine("Z = " + z);
+
+            Console.WriteLine("Результат = " + DataService.Calc(x, y, z));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tyuiu.KlokovIA.Sprint0.TaskReview.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KlokovIA.Sprint0.TaskReview.V0.Lib;
+
+namespace Tyuiu.KlokovIA.Sprint0.TaskReview.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            Assert.AreEqual(45, DataService.Calc(1, 3, 5));
+        }
+    }
+}
